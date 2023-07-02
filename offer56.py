9065c40f5f359a9a57affae1b0c7eab874d01660
wenhaofang <wenhaofang1998@qq.com>
2023-07-02 20:48:44 +0800

    add: offer56

--- offer56.py
+++ offer56.py
@@ -1,13 +1,13 @@
 '''
 Author: wenhaofang
-Date: 2023-05-11
+Date: 2023-07-02
 Description: offer56 - 数组中数字出现的次数
 '''
 
 import collections
 import functools
 
-'''思路一：位运算
+'''思路一：位运算 I
 
 时间复杂度: O(n)
 空间复杂度: O(1)
@@ -33,6 +33,35 @@ class Solution(object):
             else:
                 y ^= num
         return [x, y]
+
+'''思路一：位运算 II
+
+时间复杂度: O(n)
+空间复杂度: O(1)
+'''
+
+# class Solution(object):
+#     def singleNumber(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         bits = [0] * 22
+#         for num in nums:
+#             tmp = num
+#             pos = 0
+#             while tmp != 0:
+#                 bits[pos] += tmp % 3
+#                 tmp //= 3 #!
+#                 pos += 1
+
+#         ans = 0
+#         tmp = 1
+#         for bit in bits:
+#             ans += tmp * (bit % 3)
+#             tmp *= 3
+
+#         return ans
 
 if  __name__ == '__main__':
     solution = Solution()
